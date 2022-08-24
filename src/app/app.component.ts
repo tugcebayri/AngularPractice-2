2987a8d3174c9905af5a3c4447176a4e7c204a9d
Tugce Bayri <tugcebayri@Tugces-MacBook-Pro.local>
2022-08-24 11:42:38 +0300

    practice-2-homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newProject2';
+  userName = '';
+
+  checkUserName(){
+    return this.userName === '';
+  }
+
+  resetUserName() {
+    this.userName = '';
+  }
 }
